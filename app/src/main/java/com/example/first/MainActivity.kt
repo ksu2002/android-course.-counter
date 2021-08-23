3d89809e52580f975846777d852b584ca86c55ac
xkxsxux <simonova278@gmail.com>
2021-08-23 22:18:44 +0500

    создала счётчик, который сохраняет значение при перевороте

---
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.first
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private var counter: TextView? = null
+    private var i: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        i = savedInstanceState?.getInt("amount") ?: 0
+
+        counter = findViewById(R.id.counter)
+        counter?.text = i.toString()
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            onClickTest()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("amount", i)
+    }
+
+    private fun onClickTest() {
+        i++
+        counter?.text = i.toString()
+    }
+
+}
