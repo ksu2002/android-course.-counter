8fdcbebfdcacba7d04c32fda28b916735f6c85d6
xkxsxux <simonova278@gmail.com>
2021-09-03 20:34:33 +0500

    создала переменную test

--- app/src/main/java/com/example/first/MainActivity.kt
+++ app/src/main/java/com/example/first/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     private var counter: TextView? = null
     private var i: Int = 0
-
+    private var test: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
